620b2e530757604e687dcb1dcd5d3a412bde2fd6
Jackson Reynolds <jwreynold@gmail.com>
2020-07-07 18:31:29 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -11,11 +11,11 @@ def hello_t(array)
   end #if
 end #hello_t
 
-# call your method here!
-arr = ["Tim", "Tom", "Jim"]
-
-hello_t(arr) do |name|
-  if name.start_with?("T")
-    puts "hi #{name}"
-  end #if
-end #do
+# # call your method here!
+# arr = ["Tim", "Tom", "Jim"]
+#
+# hello_t(arr) do |name|
+#   if name.start_with?("T")
+#     puts "hi #{name}"
+#   end #if
+# end #do
